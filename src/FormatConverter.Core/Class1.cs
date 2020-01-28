12e5ee15d61c39912484237c2e1eac8e4ae9b6a7
Damir Mansurov <dmansurov@cartat.ru>
2020-01-28 09:50:06 +0300

    added syncfusion library

---
+++ src/FormatConverter.Core/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using Syncfusion.DocIORenderer;
+using Syncfusion.OfficeChart;
+
+namespace FormatConverter.Core
+{
+    public class Converter
+    {
+        public async Task Convert(Stream stream)
+        {
+            var wordDocument = new WordDocument(stream, FormatType.Docx);
+            //var selection = wordDocument.Find(, false, false);
+            wordDocument.ReplaceSingleLine("${name}","damir", false, false);
+            var render = new DocIORenderer();
+            render.Settings.ChartRenderingOptions.ImageFormat =  ExportImageFormat.Jpeg;
+            var pdfDocument = render.ConvertToPDF(wordDocument);
+            var outputStream = new MemoryStream();
+            pdfDocument.Save(outputStream);
+            using (var fs = new FileStream("Template12.pdf", FileMode.Create, FileAccess.Write))
+            {
+                outputStream.WriteTo(fs);
+            }
+            stream.Dispose();
+        }
+    }
+}
